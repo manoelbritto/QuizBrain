6f1392ebd9e42601013683af33e300371dac50ab
Manoel Britto <manoelbritto@gmail.com>
2023-02-20 22:27:45 -0500

    Initial commit

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from question_model import Question
 from quiz_brain import QuizBrain
 from data import question_data
 
-
 question_bank = []
 for question in question_data:
     question_new = Question(question['text'],  question['answer'])
     question_bank.append(question_new)
 
-print(question_bank[0].answer)
+
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
-    print(quiz.next_question())
+    quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,10 +3,23 @@ class QuizBrain:
     def __init__(self, question_bank):
         self.question_number = 0
         self.question_list = question_bank
+        self.score = 0
 
     def next_question(self):
-        input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False)? ")
+        correct_answer = self.question_list[self.question_number].answer
+        current_answer = input(f"Q.{self.question_number + 1}: "
+                               f"{self.question_list[self.question_number].text} (True/False)? ")
         self.question_number += 1
+        self.check_answer(current_answer, correct_answer)
 
     def still_has_question(self):
         return len(self.question_list) > self.question_number
+
+    def check_answer(self, u_answer, c_answer):
+        if u_answer.lower() == c_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That is wrong.")
+        print(f"The correct answer is {c_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
