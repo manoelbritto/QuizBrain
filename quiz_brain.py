9b0a9297e1b8cd20e9aa7e1a0001a72b81756258
Manoel Britto <manoelbritto@gmail.com>
2023-02-20 21:31:33 -0500

    Initial commit

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,7 @@
 class QuizBrain:
     def __init__(self, question_bank):
-        question_number = 0
-        question_list = question_bank
+        self.question_number = 0
+        self.question_list = question_bank
+
+    def next_question(self):
+        print(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False)? ")
